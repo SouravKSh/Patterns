25242dbafd0ecd4254f3cabaa0af5cadd7c82b7b
Sourav <ssouravkumar25@gmail.com>
2023-09-14 01:21:02 +0530

    good problem to solve again

--- pattern25.java
+++ pattern25.java
@@ -1,6 +1,6 @@
 public class pattern25 {
     public static void main(String[] args) {
-        int n = 8, count = 0;
+        int n = 5, count = 0;
         for (int row = 0; row < n; row++) {
             //left spaces
             for (int space = 0; space < n - 1 - row; space++)
