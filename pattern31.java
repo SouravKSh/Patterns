349000144cb638dc0a38b1c788cbe485f7b86314
Sourav <ssouravkumar25@gmail.com>
2023-09-16 21:52:25 +0530

    difficulty with observation, pls solve again later

--- pattern31.java
+++ pattern31.java
@@ -3,7 +3,9 @@ public class pattern31 {
         int n = 4;
         for (int row = 0; row < 2 * n - 1; row++) {
             for (int col = 0; col < 2 * n - 1; col++) {
-                int num = n + 1 - Math.min(Math.min(col + 1, 2 * n - col - 1), Math.min(row + 1, 2 * n - row - 1));
+                int leftRight = Math.min(col + 1, 2 * n - col - 1);
+                int topBottom = Math.min(row + 1, 2 * n - row - 1);
+                int num = n + 1 - Math.min(leftRight, topBottom);
                 System.out.print(num + " ");
             }
             System.out.println();
