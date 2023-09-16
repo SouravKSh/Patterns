5856b5e4154b2a1ac1832b4374550b7cba68e351
Sourav <ssouravkumar25@gmail.com>
2023-09-16 22:14:10 +0530

    small and big character pattern

---
+++ pattern23.java
@@ -0,0 +1,12 @@
+public class pattern23 {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int row = 0; row < n; row++) {
+
+        }
+    }
+}
+
+//        *      *   
+//      *   *  *   * 
+//    *      *      *
